3dc640e86ebbe432037e1d161126f42a2aaa718b
tsurubaso <tsurubaso@gmail.com>
2023-04-05 17:42:34 +0900

    update auteur

--- src/views/PresentationAuteur.vue
+++ src/views/PresentationAuteur.vue
@@ -6,17 +6,16 @@
   </div>
   </head>
  
-
-
-    
-
   <body>
     <div class="page">
       
       <h1>Présentation</h1>
-      <p>Sylvain Bilhaud est une écrivain français, célèbre surtout pour ses avis critiques sur le Japon, son autre nation. Né dans une famille compliquée mais aimante, il a suivi une éducation en France, mais les complexes qu'il avait vis à vis de l'écriture l'on longtemps empéché d'écrire.
-        Les réseaux sociaux l'aideront à faire face à son désir d'écriture. Sylvain Bilhaud est considéré comme l'un des auteur les plus énigmatique et mystrérieux de son temps.
-        Malgré de nombreuses difficultés il continura et continue encore à écrire, comme il le dit lui-même. " Ne voyant aucun moyen de faire autrement".</p>
+      <p>Sylvain Bilhaud est une écrivain français, célèbre surtout pour ses avis critiques sur le Japon, son engagement politique discret lors du conflit Ukrainien. Né dans une famille compliquée mais aimante, il a suivi une éducation en France, mais les complexes qu'il avait vis à vis de l'écriture l'ont longtemps empêché d'écrire.
+        Les réseaux sociaux l'aideront à faire face à son désir d'écriture. Sylvain Bilhaud est considéré comme l'un des auteurs les plus énigmatiques et mystérieux de son temps.
+Pratiquement inconnu du grand public et loin des grands médias, il a su se construire une petite troupe de fervents lecteurs.
+Son style est simple, parfois mêlé de vulgarité, mais son propos si il est suivi jusqu’au bout à la profondeur.
+        Malgré de nombreuses difficultés, il continue encore à écrire, comme il le dit lui-même. " Ne voyant aucun moyen de faire autrement".
+</p>
     </div>
   </body>
 
